f1874a321228f99f7f83b5d66faec15d70ad0e3d
Victor Goudkov <vgoudkov@iitdgroup.ru>
2020-04-11 10:59:15 +0300

    Task 1

---
+++ src/main/java/ru/iitdgroup/nir/ChangeBiggestAndSmallest.java
@@ -0,0 +1,27 @@
+package ru.iitdgroup.nir;
+
+/**
+ * The exercise: Change the position of the biggest and the smallest number in the array
+ */
+@SuppressWarnings({"squid:S106"})
+public class ChangeBiggestAndSmallest {
+    public static void main(String[] args) {
+        int[] data = new int[]{
+                1, 2, 3, 4, 5, 28, 3, 14, 2, 12, -42, 31, 15
+        };
+        System.out.println("Original: " + Util.toString(data));
+        swapBiggestAndSmallest(data);
+        System.out.println(" Swapped: " + Util.toString(data));
+    }
+
+    /**
+     * Change the position of the biggest and the smallest number in the array
+     *
+     * @param data array to work with
+     */
+    static void swapBiggestAndSmallest(int[] data) {
+        /*
+         ===== CREATE CODE HERE =====
+         */
+    }
+}
---
+++ src/main/java/ru/iitdgroup/nir/Util.java
@@ -0,0 +1,27 @@
+package ru.iitdgroup.nir;
+
+/**
+ * Misc utils
+ */
+public class Util {
+    /**
+     * Print an in array human-readable way
+     * @param data array to print
+     * @return String. An array in format {1,1,2,3,5}
+     */
+    public static String toString(int[] data) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 0; i < data.length; i++) {
+            sb.append(data[i]);
+            if (i < data.length - 1) sb.append(", ");
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+    
+    private Util(){
+        //do not create instance of this class
+    }
+    
+}
